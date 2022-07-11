52053c65f58555d2b78ffd08c90b4f9e9b6d249d
Zsolt <npkd@protonmail.com>
2022-07-12 00:36:19 +0300

    CS-215 fixed validation message positioning

--- csatar-plugins/forms/assets/js/positionValidationTags.js
+++ csatar-plugins/forms/assets/js/positionValidationTags.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
     $('#validationTags').children().each(function () {
         if($(this).data( "validateFor" )){
             let inputName = 'data[' + $(this).data( "validateFor" ) + ']';
-            $(this).insertAfter( '[name="' + inputName + '"]' );
+            let inputTag = $('[name="' + inputName + '"]')
+            $(this).insertAfter( inputTag.parent().children().last() );
         }
     });
 
