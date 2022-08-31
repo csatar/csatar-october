5e1a897acec519aaef1c4e31b19dae05dfb82cb6
Zsolt <npkd@protonmail.com>
2022-08-31 21:26:26 +0300

    CS-290 - Fixed bugs - Updated change log

--- csatar-plugins/csatar/classes/RightsMatrix.php
+++ csatar-plugins/csatar/classes/RightsMatrix.php
@@ -7,7 +7,8 @@ use Db;
 class RightsMatrix
 {
     public static function getRightsMatrixLastUpdateTime() {
-        return Db::table('csatar_csatar_mandates_permissions')->
-            select('updated_at')->orderBy('updated_at','DESC')->first()->updated_at;
+        $lastUpdated = Db::table('csatar_csatar_mandates_permissions')->
+        select('updated_at')->orderBy('updated_at','DESC')->first();
+        return $lastUpdated->updated_at ?? new \DateTime();
     }
 }
