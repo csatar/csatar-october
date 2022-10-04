5c4d78befd110c32670714e377d0c0f3e1848630
Zsolt <npkd@protonmail.com>
2022-10-05 00:17:17 +0300

    CS-338 - Reduced EcsetCode length to 6 characters + separator + suffix. - Fixed bug on form builder

--- csatar-plugins/csatar/models/Scout.php
+++ csatar-plugins/csatar/models/Scout.php
@@ -396,13 +396,22 @@ class Scout extends OrganizationBase
 
         $sufix = $team->district->association->ecset_code_suffix ?? substr($team->district->association->name, 0, 2);
 
-        $ecset_code = strtoupper(substr(uniqid(), 0, -3) . '-' . $sufix);
+        $ecset_code = strtoupper(substr(uniqid(), 0, 6) . '-' . $sufix);
 
-        if(Scout::where('ecset_code', $ecset_code)->exists()){
+        if($this->ecsetCodeExists($ecset_code)){
+            $ecset_code = strtoupper(substr(uniqid(), 1, 6) . '-' . $sufix);
+        }
+
+        if($this->ecsetCodeExists($ecset_code)){
             return $this->generateEcsetCode();
         }
 
         return $ecset_code;
+    }
+
+    private function ecsetCodeExists(string $code): bool
+    {
+        return Scout::where('ecset_code', $code)->exists();
     }
 
     private function validatePivotDateAndLocationFields($fields, $category)
