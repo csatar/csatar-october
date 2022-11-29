5ae2f2ae56560047d66ae4cf9596011a4312679a
jozsitoth96 <jozsef.toth96@gmail.com>
2022-11-29 10:27:37 +0200

    CS-319 - Added new files for localization data

---
+++ csatar-plugins/csatar/updates/builder_table_create_csatar_csatar_locations.php
@@ -0,0 +1,27 @@
+<?php namespace Csatar\Csatar\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateCsatarCsatarLocations extends Migration
+{
+    public function up()
+    {
+        Schema::create('csatar_csatar_locations', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->string('country', 255)->nullable();
+            $table->string('county', 255)->nullable();
+            $table->string('city', 255)->nullable();
+            $table->string('street_type', 255)->nullable();
+            $table->string('street', 255)->nullable();
+            $table->string('number', 255)->nullable();
+            $table->string('code', 255)->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('csatar_csatar_locations');
+    }
+}
