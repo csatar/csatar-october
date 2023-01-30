24a0d6dfb3d2fea17b608c54b54750b8f8ded60d
zsso <78356157+zsso@users.noreply.github.com>
2023-01-30 11:09:19 +0200

    Cs-458 make all buttons uniform (#232)
    * CS-458 - Added buttons partial
    - Buttons partial can handle passed permission value
    - Changed buttons on every component to one of the buttons partial, expect gallery
    
    * CS-358 - Added tooltip to form buttons where 2fa is needed
    - Added icons to form buttons where they were missing

--- csatar-plugins/csatar/components/TeamReports.php
+++ csatar-plugins/csatar/components/TeamReports.php
@@ -60,6 +60,7 @@ class TeamReports extends ComponentBase
                         'submitted_at'  => (new DateTime($teamReport->submitted_at))->format('Y-m-d'),
                     ];
                 }
+                $this->permissions[$teamReport->id] = Auth::user()->scout->getRightsForModel($teamReport)['MODEL_GENERAL'] ?? null;
             }
         }
         elseif ($this->id == null) {
@@ -85,6 +86,7 @@ class TeamReports extends ComponentBase
                         'link_text' => isset($teamReport->submitted_at) ? Lang::get('csatar.csatar::lang.plugin.component.teamReports.view') : Lang::get('csatar.csatar::lang.plugin.component.teamReports.edit'),
                     ];
                 }
+                $this->permissions[$teamReport->id] = Auth::user()->scout->getRightsForModel($teamReport)['MODEL_GENERAL'] ?? null;
             }
             $this->teamReportData = collect($this->teamReportData);
             $this->teamReportData = $this->teamReportData->sortBy(function ($teamReport) {
