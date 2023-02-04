a5a156fb0f7ede83ee80fadab2e2381841a4dbef
Zsolt <npkd@protonmail.com>
2023-02-04 20:53:31 +0200

    Added getLogoThumbAttribute method to team

--- csatar-plugins/csatar/models/Team.php
+++ csatar-plugins/csatar/models/Team.php
@@ -181,6 +181,11 @@ class Team extends OrganizationBase
         return $value ? $value : '/plugins/csatar/csatar/assets/images/default-team-logo.png';
     }
 
+    public function getLogoThumbAttribute($value)
+    {
+        return $value ? $value : '/plugins/csatar/csatar/assets/images/default-team-logo.png';
+    }
+
     public function beforeSave()
     {
         $filterWords = explode(',', Lang::get('csatar.csatar::lang.plugin.admin.team.filterOrganizationUnitNameForWords'));
