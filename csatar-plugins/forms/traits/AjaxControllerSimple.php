02df9d1f1f7e79d54247f8e6051663088672cfa2
Zsolt <npkd@protonmail.com>
2023-02-28 01:11:04 +0200

    CS-387 Temp commit

--- csatar-plugins/forms/traits/AjaxControllerSimple.php
+++ csatar-plugins/forms/traits/AjaxControllerSimple.php
@@ -54,6 +54,10 @@ trait AjaxControllerSimple {
                 'label' => 'MarkdownEditor',
                 'code'  => 'markdown'
             ],
+            'Backend\FormWidgets\TagList' => [
+                'label' => 'TagList',
+                'code'  => 'taglist'
+            ],
             // Custom file upload for frontend use
             'Csatar\Forms\Widgets\FrontendFileUpload' => [
                 'label' => 'FileUpload',
