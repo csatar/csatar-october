4e89bbc9f8ada68e794ac65e008460ce8c32ca88
Zsolt <npkd@protonmail.com>
2023-03-24 10:08:46 +0200

    CS-514 - Added trial system to Methodologies - Added all columns to trial system frontend table - Added fixed from layout for game and methodology - Removed word "Próbarendszer" from lables where not necessary

---
+++ csatar-plugins/knowledgerepository/updates/builder_table_create_csatar_methodology_trial_system.php
@@ -0,0 +1,25 @@
+<?php namespace Csatar\KnowledgeRepository\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateCsatarMethodologyTrialSystem extends Migration
+{
+    public function up()
+    {
+        Schema::create('csatar_methodology_trial_system', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->integer('methodology_id')->unsigned();
+            $table->integer('trial_system_id')->unsigned();
+            
+            $table->foreign('methodology_id', 'trial_system_methodology_id_foreign')->references('id')->on('csatar_knowledgerepository_methodologies');
+            $table->foreign('trial_system_id', 'methodology_trial_system_id_foreign')->references('id')->on('csatar_knowledgerepository_trial_systems');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('csatar_methodology_trial_system');
+    }
+}
