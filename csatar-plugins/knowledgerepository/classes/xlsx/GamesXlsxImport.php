ef9e20784bdbffebcbb16b1f007e105d0e41eff0
Zsolt <npkd@protonmail.com>
2023-03-29 12:18:02 +0300

    CS-529 Added Games xlsx import functionality

---
+++ csatar-plugins/knowledgerepository/classes/xlsx/GamesXlsxImport.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Csatar\KnowledgeRepository\Classes\Xlsx;
+
+use Csatar\Csatar\Models\AgeGroup;
+use Csatar\KnowledgeRepository\Models\Duration;
+use Csatar\KnowledgeRepository\Models\Game;
+use Csatar\KnowledgeRepository\Models\GameDevelopmentGoal;
+use Csatar\KnowledgeRepository\Models\GameType;
+use Csatar\KnowledgeRepository\Models\Headcount;
+use Csatar\KnowledgeRepository\Models\Location;
+use Csatar\KnowledgeRepository\Models\Tool;
+use Csatar\KnowledgeRepository\Models\TrialSystem;
+use Db;
+use Illuminate\Support\Collection;
+use Illuminate\Validation\Validator;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Concerns\RemembersRowNumber;
+use Maatwebsite\Excel\Concerns\SkipsFailures;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\SkipsUnknownSheets;
+use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
+use Lang;
+
+
+class GamesXlsxImport implements ToModel, WithHeadingRow, WithValidation, SkipsOnFailure, WithMultipleSheets, SkipsUnknownSheets
+{
+    use Importable, RemembersRowNumber, SkipsFailures;
+
+    private $associationId;
+    private $uploaderCsatarCode;
+    private $approverCsatarCode;
+
+    private $overwrite = false;
+
+    public $errors = [];
+
+    public function __construct($associationId, $uploaderCsatarCode, $approverCsatarCode, $overwrite = false)
+    {
+        $this->associationId = $associationId;
+        $this->uploaderCsatarCode = $uploaderCsatarCode;
+        $this->approverCsatarCode = $approverCsatarCode;
+        $this->overwrite = $overwrite;
+    }
+
+    public function sheets(): array
+    {
+        return [
+            0 => $this,
+        ];
+    }
+
+    public function onUnknownSheet($sheetName)
+    {
+        // E.g. you can log that a sheet was not found.
+        info("Sheet {$sheetName} was skipped");
+    }
+
+    public function model(array $row)
+    {
+        if (!isset($row['jatek_neve'])) {
+            return null;
+        }
+
+        $game = Game::where('association_id', $this->associationId)
+            ->where('name', $row['jatek_neve'])
+            ->first();
+
+        if (!$this->overwrite && !empty($game)) {
+            $this->errors[$this->getRowNumber()][] = Lang::get('csatar.knowledgerepository::lang.plugin.admin.game.gameAlreadyExists', ['name' => $row['jatek_neve']]);
+            return;
+        }
+
+        if (empty($game)) {
+            $game = new Game();
+            $game->association_id = $this->associationId;
+            $game->name = $row['jatek_neve'];
+        }
+
+        $pivotRelationIds = [];
+
+        if (isset($row['letszam'])) {
+            $pivotRelationIds['headcounts'] = $this->getModelIds($row['letszam'], Headcount::class, 'description');
+        }
+
+        if (isset($row['idotartam'])) {
+            $pivotRelationIds['durations'] = $this->getModelIds($row['idotartam'], Duration::class, 'name');
+        }
+
+        if (isset($row['korosztaly'])) {
+            $pivotRelationIds['age_groups'] = $this->getModelIds($row['korosztaly'], AgeGroup::class, 'name', 'association_id', $this->associationId);
+        }
+
+        if (isset($row['helyszin'])) {
+            $pivotRelationIds['locations'] = $this->getModelIds($row['helyszin'], Location::class, 'name');
+        }
+
+        if (isset($row['cel'])) {
+            $pivotRelationIds['game_development_goals'] = $this->getModelIds($row['cel'], GameDevelopmentGoal::class, 'name');
+        }
+
+        if (isset($row['tipus'])) {
+            $pivotRelationIds['game_types'] = $this->getModelIds($row['tipus'], GameType::class, 'name');
+        }
+
+        if (isset($row['probarendszer'])) {
+            $pivotRelationIds['trial_systems'] = $this->getModelIds($row['probarendszer'], TrialSystem::class, 'id_string', 'association_id', $this->associationId);
+        }
+
+        if (isset($row['kellekek'])) {
+            $pivotRelationIds['tools'] = $this->getModelIds($row['kellekek'], Tool::class, 'name', null, null, true);
+        }
+
+        if (!empty($this->errors[$this->getRowNumber()])) {
+            return;
+        }
+
+        $game->update([
+            'description' => $row['leiras'] ?? null,
+            'link' => $row['link'] ?? null,
+            'uploader_csatar_code' => $this->uploaderCsatarCode,
+            'approver_csatar_code' => $this->approverCsatarCode,
+            'note' => $row['megjegyzes'],
+        ]);
+
+        $game->save();
+
+        $this->syncRelations($game, $pivotRelationIds);
+    }
+
+    public function headingRow(): int
+    {
+        return 1;
+    }
+
+    public function syncRelations(Game $game, array $pivotRelationIds)
+    {
+        foreach ($pivotRelationIds as $relationName => $relationIds) {
+            $game->{$relationName}()->sync($relationIds);
+        }
+    }
+
+    public function getModelIds(string $searchFor, string $modelName, string $columnName, string $secondaryColumnName = null, $secondaryColumnValue = null, bool $createIfNotFound = false): array
+    {
+        $searchFor = array_map('trim', explode('|', $searchFor));
+        $searchFor = array_map('strtolower', $searchFor);
+        $ids = $modelName::whereIn(DB::raw('LOWER(' . $columnName . ')'), $searchFor)->when($secondaryColumnName, function ($query) use ($secondaryColumnName, $secondaryColumnValue) {
+            $query->where($secondaryColumnName, $secondaryColumnValue);
+        })->get();
+        $unmatched = array_diff($searchFor, array_map('strtolower', $ids->pluck($columnName)->toArray()));
+        if (!empty($unmatched) && !$createIfNotFound) {
+            $modelNameForLangKey = (new \ReflectionClass($modelName))->getShortName();
+            $this->errors[$this->getRowNumber()][] = Lang::get('csatar.knowledgerepository::lang.plugin.admin.messages.cannotFind' . $modelNameForLangKey) . implode(', ', $unmatched);
+        }
+        if ($createIfNotFound && !empty($unmatched)) {
+            foreach ($unmatched as $unmatchedItem) {
+                $model = new $modelName();
+                $model->$columnName = $unmatchedItem;
+                if ($secondaryColumnName) {
+                    $model->$secondaryColumnName = $secondaryColumnValue;
+                }
+                $model->save();
+                $ids->push($model);
+            }
+        }
+
+        return $ids->pluck('id')->toArray();
+    }
+
+    public function rules(): array
+    {
+        return [
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            if (!empty($this->errors) && isset($this->errors[$this->getRowNumber()])) {
+                $validator->errors()->add($this->getRowNumber(), $this->errors[$this->getRowNumber()]);
+            }
+        });
+    }
+}
--- csatar-plugins/knowledgerepository/controllers/Games.php
+++ csatar-plugins/knowledgerepository/controllers/Games.php
@@ -1,7 +1,15 @@
 <?php namespace Csatar\KnowledgeRepository\Controllers;
 
-use Backend\Classes\Controller;
 use BackendMenu;
+use Backend\Classes\Controller;
+use Csatar\Csatar\Models\Association;
+use Csatar\KnowledgeRepository\Classes\Xlsx\GamesXlsxImport;
+use Db;
+use Flash;
+use Input;
+use Vdomah\Excel\Classes\Excel;
+use Validator;
+use Lang;
 
 class Games extends Controller
 {
@@ -17,9 +25,95 @@ class Games extends Controller
         'csatar.manage.data'
     ];
 
+    public $errors = [];
+
     public function __construct()
     {
         parent::__construct();
         BackendMenu::setContext('Csatar.KnowledgeRepository', 'main-menu-knowledge-repository', 'side-menu-games');
+        $this->pageTitle = Lang::get('csatar.knowledgerepository::lang.plugin.admin.general.import');
+    }
+
+    public function import()
+    {
+    }
+
+    public function onGetScoutOptions() {
+        $searchTerm = post('term');
+        $queryResults = Db::table('csatar_csatar_scouts')->whereRaw("CONCAT(family_name, ' ', given_name, ' ', ecset_code) like ?", ['%'.$searchTerm.'%'])->paginate(15);
+        $results = [];
+        foreach ($queryResults as $result) {
+            $results[] = [
+                'id' => $result->ecset_code,
+                'text' => $result->family_name . ' ' . $result->given_name . ' - ' . $result->ecset_code,
+            ];
+        }
+
+        return [
+            'results' => $results,
+            'pagination' => [
+                'more' => true
+            ]
+        ];
+    }
+
+    public function onImportGames(){
+        $rules = [
+            'association_id' => 'required',
+            'uploader_csatar_code' => 'required',
+            'xlsx_file' => 'required',
+        ];
+
+        $attributeNames = [
+            'association_id' => Lang::get('csatar.csatar::lang.plugin.admin.association.association'),
+            'uploader_csatar_code' => Lang::get('csatar.knowledgerepository::lang.plugin.admin.game.uploader'),
+            'xlsx_file' => Lang::get('csatar.knowledgerepository::lang.plugin.admin.general.file'),
+        ];
+
+        $validator = Validator::make(Input::all(), $rules, [], $attributeNames);
+        if ($validator->fails()) {
+            throw new \ValidationException($validator);
+        }
+
+        $associationId = Input::get('association_id');
+        $uploaderCsatarCode = Input::get('uploader_csatar_code');
+        $approverCsatarCode = Input::get('approver_csatar_code');
+        $owerwriteExistingData = Input::get('overwrite_existing_data');
+        $xlsxFile = Input::file('xlsx_file');
+
+        if (empty($xlsxFile) || !$xlsxFile->isValid() || ($xlsxFile->getMimeType() != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')) {
+            return; //TODO exception here
+        }
+
+        $xlsxFile = $xlsxFile->move(temp_path(), $xlsxFile->getClientOriginalName());
+
+        $import = new GamesXlsxImport($associationId, $uploaderCsatarCode, $approverCsatarCode, $owerwriteExistingData);
+        $import->import($xlsxFile);
+
+        if (!empty($import->failures()) && count($import->failures()) > 0) {
+            $this->vars['errors'] = $import->failures();
+
+            return [
+                '#errors' => $this->makePartial('import_errors'),
+            ];
+        }
+
+        Flash::success(Lang::get('csatar.knowledgerepository::lang.plugin.admin.messages.importSuccessful'));
+    }
+
+    public function onGetAssociationOptions() {
+        $associations = Association::all()->lists('name', 'id');
+        $results = [];
+        foreach ($associations as $id => $name) {
+            $results[] = [
+                'id' => $id,
+                'text' => $name,
+                'selected' => $name == 'Romániai Magyar Cserkészszövetség' ? true : false,
+            ];
+        }
+
+        return [
+            'results' => $results,
+        ];
     }
 }
