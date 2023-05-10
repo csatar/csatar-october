da5b01ad2757d3ff00a31449a9994c7e6d8f6434
Zsolt <npkd@protonmail.com>
2023-05-10 17:15:36 +0300

    CS-582 - Fixed Troop creation bug caused by form builder - Made troop status required

--- csatar-plugins/csatar/models/Troop.php
+++ csatar-plugins/csatar/models/Troop.php
@@ -38,6 +38,7 @@ class Troop extends OrganizationBase
         'facebook_page' => 'url|regex:(facebook)|nullable',
         'logo' => 'image|nullable',
         'team' => 'required',
+        'status' => 'required',
     ];
 
     /**
