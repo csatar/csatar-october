cd5c270e39d30040f28d95c0c50107c562269397
codefactor-io <support@codefactor.io>
2023-05-17 18:13:38 +0000

    [CodeFactor] Apply fixes

--- csatar-plugins/knowledgerepository/updates/builder_table_create_csatar_knowledgerepository_weekly_work_plan_material.php
+++ csatar-plugins/knowledgerepository/updates/builder_table_create_csatar_knowledgerepository_weekly_work_plan_material.php
@@ -1,10 +1,12 @@
-<?php namespace Csatar\KnowledgeRepository\Updates;
+<?php
+namespace Csatar\KnowledgeRepository\Updates;
 
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
 class BuilderTableCreateCsatarKnowledgerepositoryWeeklyWorkPlanMaterial extends Migration
 {
+
     public function up()
     {
         Schema::create('csatar_knowledgerepository_weekly_work_plan_material', function($table)
@@ -13,15 +15,16 @@ class BuilderTableCreateCsatarKnowledgerepositoryWeeklyWorkPlanMaterial extends 
             $table->integer('weekly_work_plan_id')->unsigned();
             $table->integer('new_material_id')->nullable()->unsigned();
             $table->integer('old_material_id')->nullable()->unsigned();
-            
+
             $table->foreign('weekly_work_plan_id', 'weekly_work_plan_foreign')->references('id')->on('csatar_knowledgerepository_weekly_work_plans');
             $table->foreign('new_material_id', 'weekly_work_plan_new_material_foreign')->references('id')->on('csatar_knowledgerepository_trial_systems');
             $table->foreign('old_material_id', 'weekly_work_plan_old_material_foreign')->references('id')->on('csatar_knowledgerepository_trial_systems');
         });
     }
-    
+
     public function down()
     {
         Schema::dropIfExists('csatar_knowledgerepository_weekly_work_plan_material');
     }
+
 }
