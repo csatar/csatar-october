cd9862f0dd15f20297b93a089124d1e91bd478e4
Zsolt <npkd@protonmail.com>
2023-05-19 00:24:20 +0300

    CS-564 - CodeFactor issue fix

--- csatar-plugins/forms/widgets/taglist/HasRelationStore.php
+++ csatar-plugins/forms/widgets/taglist/HasRelationStore.php
@@ -32,7 +32,6 @@ trait HasRelationStore
                 })
                 ->all();
         }
-
         // Take existing relationship
         else {
             $result = $this->getRelationObject()->pluck($this->relationKey)->all();
@@ -82,7 +81,6 @@ trait HasRelationStore
                 ->all()
             ;
         }
-
         // Options from model
         else {
             $existingTags = $relationModel
