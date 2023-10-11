24a13a0f2af73468259dc21dce2b18957a0e3159
zsso <78356157+zsso@users.noreply.github.com>
2023-10-11 17:03:13 +0300

    CS-602 - Showing structure items based on permissions (#379)
    * CS-602 - Showing structure item in accordion and menu for guest user based on permissions

--- csatar-plugins/csatar/classes/StructureTree.php
+++ csatar-plugins/csatar/classes/StructureTree.php
@@ -239,12 +239,82 @@ class StructureTree
         }
 
         $structureTree = Cache::pull('structureTree');
-            if (empty($structureTree)) {
-                StructureTree::getStructureTree();
-                return;
-            }
+        if (empty($structureTree)) {
+            StructureTree::getStructureTree();
+            return;
+        }
+
+        $refreshedAssociation['associationTreePermissions'] = self::setassociationTreePermissions($associationId);
 
         $structureTree[$associationId] = $refreshedAssociation;
+
+        Cache::forever('structureTree', $structureTree);
+    }
+
+    public static function setassociationTreePermissions($associationId): array
+    {
+        $association = Association::find($associationId);
+
+        $district = new District();
+        $district->association = $association;
+
+        $team           = new Team();
+        $team->district = $district;
+
+        $troop       = new Troop();
+        $troop->team = $team;
+
+        $patrol       = new Patrol();
+        $patrol->team = $team;
+
+        $scout       = new Scout();
+        $scout->team = $team;
+
+        return [
+            'association' => [
+                'guest' => [
+                    'read' => $association->getGuestRightsForModel()['MODEL_GENERAL']['read'] ?? 0,
+                ],
+            ],
+            'district' => [
+                'guest' => [
+                    'read' => $district->getGuestRightsForModel()['MODEL_GENERAL']['read'] ?? 0,
+                ],
+            ],
+            'team' => [
+                'guest' => [
+                    'read' => $team->getGuestRightsForModel()['MODEL_GENERAL']['read'] ?? 0,
+                ],
+            ],
+            'troop' => [
+                'guest' => [
+                    'read' => $troop->getGuestRightsForModel()['MODEL_GENERAL']['read'] ?? 0,
+                ],
+            ],
+            'patrol' => [
+                'guest' => [
+                    'read' => $patrol->getGuestRightsForModel()['MODEL_GENERAL']['read'] ?? 0
+                ],
+            ],
+            'scout' => [
+                'guest' => [
+                    'read' => $scout->getGuestRightsForModel()['MODEL_GENERAL']['read'] ?? 0,
+                ],
+            ],
+        ];
+    }
+
+    public static function updateassociationTreePermissions() {
+        $structureTree = Cache::pull('structureTree');
+        if (empty($structureTree)) {
+            StructureTree::getStructureTree();
+            return;
+        }
+
+        foreach ($structureTree as $associationId => $association) {
+            $structureTree[$associationId]['associationTreePermissions'] = self::setassociationTreePermissions($associationId);
+        }
+
         Cache::forever('structureTree', $structureTree);
     }
 
